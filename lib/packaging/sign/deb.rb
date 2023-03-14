500c4bf532d0406292a11b04b3f6f6a1a57875cf
isaac-hammes <isaac.hammes@puppet.com>
2023-03-14 13:33:21 -0700

    test

--- lib/packaging/sign/deb.rb
+++ lib/packaging/sign/deb.rb
@@ -1,9 +1,12 @@
 module Pkg::Sign::Deb
   module_function
 
-  def sign_changes(file)
+  def sign_changes(change_file)
     # Lazy lazy lazy lazy lazy
     sign_program = "-p'gpg --use-agent --no-tty'" if ENV['RPM_GPG_AGENT']
-    Pkg::Util::Execution.capture3("debsign #{sign_program} --re-sign -k#{Pkg::Config.gpg_key} #{file}")
+    Pkg::Util::Execution.capture3(
+      "debsign #{sign_program} --re-sign -k#{Pkg::Config.gpg_key} #{change_file}",
+      true
+    )
   end
 end
